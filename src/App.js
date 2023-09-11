c33362262c1c3f26a5c792f9b1c5b10e9ec8540c
Zelimkhan Magomadov <zelimkhan.m21@gmail.com>
2023-09-12 00:37:08 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [inputText, setInputText] = useState("");
+
+  function onInputTextChange(text) {
+    setInputText(text);
+  }
+
+  let [textItems, setTextItem] = useState([]);
+
+  function onClickTextSave() {
+    setTextItem([...textItems, { text: inputText, isDone: false }]);
+    setInputText("");
+  }
+
+  function onClickTextRemove(id) {
+    const newItems = textItems.filter((item, _id) => _id !== id);
+    setTextItem([...newItems]);
+  }
+
+  let [isDone, setDone] = useState(false);
+
+  function doneToggle(id) {
+    const newArray = [...textItems];
+    newArray[id].isDone = !newArray[id].isDone;
+    setTextItem(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(event) => onInputTextChange(event.target.value)}
+        style={{ margin: "16px" }}
+      />
+      <button onClick={onClickTextSave} style={{ margin: "8px" }}>
+        Save
+      </button>
+      <ul>
+        {textItems.map((item, id) => (
+          <li
+            key={id}
+            style={{ textDecoration: item.isDone ? "line-through" : "none" }}
+          >
+            <input
+              type="checkbox"
+              checked={item.isDone}
+              onChange={() => doneToggle(id)}
+            />
+            {item.text}
+            <button
+              onClick={() => onClickTextRemove(id)}
+              style={{ margin: "8px" }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
